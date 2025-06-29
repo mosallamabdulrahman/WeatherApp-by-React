1bb5589715e5c8f977bb28fb8c1872894b02c0ab
AbdulrahmanMosallam <mosallamabdulrahman@gmail.com>
2025-06-29 18:59:46 +0300

    Initial commit

---
+++ src/weatherApiSlice.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchWeather = createAsyncThunk(
+	"weatherApi/fetchWeather",
+	async () => {
+		const response = await axios.get(
+			"https://api.openweathermap.org/data/2.5/weather?lat=24.7&lon=46.5&appid=2c511dc8a04616514a9e1ff2f31c8043"
+		);
+
+		// handle success
+		const responseTemp = Math.round(response.data.main.temp - 272.15);
+		const min = Math.round(response.data.main.temp_min - 272.15);
+		const max = Math.round(response.data.main.temp_max - 272.15);
+		const description = response.data.weather[0].description;
+		const responseIcon = response.data.weather[0].icon;
+
+		return {
+			number: responseTemp,
+			min,
+			max,
+			description,
+			icon: `https://openweathermap.org/img/wn/${responseIcon}@2x.png`,
+		};
+	}
+);
+const weatherApiSlice = createSlice({
+	name: "weatherApi",
+
+	initialState: {
+		result: "empty",
+		weather: {},
+		isLoading: false,
+	},
+
+	reducers: {
+		changeResult: (state, action) => {
+			state.result = "changed";
+		},
+	},
+
+	extraReducers(builder) {
+		builder
+			.addCase(fetchWeather.pending, (state, action) => {
+				state.isLoading = true;
+			})
+			.addCase(fetchWeather.fulfilled, (state, action) => {
+				state.isLoading = false;
+
+				state.weather = action.payload;
+			})
+			.addCase(fetchWeather.rejected, (state, action) => {
+				state.isLoading = false;
+			});
+	},
+});
+
+export const { changeResult } = weatherApiSlice.actions;
+export default weatherApiSlice.reducer;
